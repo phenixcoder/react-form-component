f6ee8c493cd711b967f5e10d91ef1661655d74e1
Balwant Singh <balwant.matharu@gmail.com>
2020-07-12 15:27:21 +1000

    Initial Commit

---
+++ src/components/Form.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Form } from "./Form";
+storiesOf("Select | Catalog", module).add("Default", () => <Form />);
